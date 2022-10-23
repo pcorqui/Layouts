7453c778aaeec33f20efb79d36e08401d806bdd8
pcorqui <pcorqui@gmail.com>
2022-10-23 10:48:23 -0500

    introduccion a linearLayour

--- app/src/main/java/com/example/layouts/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/layouts/ui/home/HomeFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.layouts.databinding.FragmentHomeBinding
+import com.example.layouts.databinding.FragmentLinearBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentLinearBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class HomeFragment : Fragment() {
         val homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentLinearBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
